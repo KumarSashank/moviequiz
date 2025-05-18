661f4b221981089c67101bb9ea52ed23576e80b8
SubhashSolleti <solletisubhash24@gmail.com>
2025-05-19 04:35:55 +0530

    added docker file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './pages/Home';
-import QuizStart from './pages/QuizStart';
-import ConnectionStatus from './pages/ConnectionStatus';
+import logo from './logo.svg';
+import './App.css';
 
-
-
-// …other pages
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/check" element={<ConnectionStatus />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/quiz/:id/start" element={<QuizStart />} />
-          {/* add /result, /my-attempts, /achievements here */}
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
