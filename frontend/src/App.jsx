d37e58649c94be716fe2d10c6d0309e4583118c3
Kumar Sashank Ghanta <kumarsashank2003@gmail.com>
2025-05-18 22:06:02 -0400

    change code for app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import QuizStart from './pages/QuizStart';
+import ConnectionStatus from './pages/ConnectionStatus';
 
-function App() {
+
+
+// …other pages
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/check" element={<ConnectionStatus />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz/:id/start" element={<QuizStart />} />
+          {/* add /result, /my-attempts, /achievements here */}
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
-
-export default App;
